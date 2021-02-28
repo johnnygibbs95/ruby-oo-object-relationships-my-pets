aa7ae7c14e783193a2ff9fbf84535a56c0a8b034
johnnygibbs95 <johnnydgibbs1995@gmail.com>
2021-02-28 13:54:17 -0800

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
-  # code goes here
+  
+  attr_accessor
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| self == cat.owner}
+  end
+
+  def dogs
+    Dog.all.select {|dog| self == dog.owner}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    Dog.all.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = Cat.all + Dog.all
+    pets.each {|pet| pet.mood = "nervous"}
+    pets.each {|pet| pet.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
